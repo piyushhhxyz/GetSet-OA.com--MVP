cce49f7218e7ca858e10ba473bd50b37a4395815
parvsehgal <parvsehgal2004@gmail.com>
2023-10-26 23:10:49 +0530

    corrected CORS error

--- server/index.js
+++ server/index.js
@@ -7,17 +7,16 @@ const passport = require("passport");
 const fileUpload = require("express-fileupload");
 const { dbConnect } = require("./config/dbConnect");
 
-
 const authRoute = require("./routes/auth");
-const uploadRoute = require("./routes/Upload")
-const getDataRoutes = require('./routes/getData');
+const uploadRoute = require("./routes/Upload");
+const getDataRoutes = require("./routes/getData");
 
 const app = express();
 
 dbConnect();
-app.use(express.static('public'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
 );
@@ -26,23 +25,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(
-	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
-	})
-)
-
-app.use(
-  cors({
-    origin: "https://www.getsetoa.tech",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
   })
 );
 
+// app.use(
+//   cors({
+//     origin: "https://www.getsetoa.tech",
+//     methods: "GET,POST,PUT,DELETE",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
+
 app.use("/auth", authRoute);
-app.use('/api/v1', uploadRoute)
-app.use('/api/v1/getData', getDataRoutes);
+app.use("/api/v1", uploadRoute);
+app.use("/api/v1/getData", getDataRoutes);
 
 app.listen(4000, () => {
   console.log("Server is running at 4000!");
